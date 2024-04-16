877ed13d8c7c96a621eee36285bde2bde91d733d
ANGELO MARCOS SIRINO PEDRO <angelo.pedro@EPM334049.PMPR>
2024-04-15 21:48:49 -0300

    criando o mailer

--- index.php
+++ index.php
@@ -170,9 +170,23 @@ $app->post("/admin/forgot", function() {
 
 	
 	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
 	
 });
 
+$app->get("/admin/forgot/sent", function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+
+	$page->setTpl("forgot-sent");
+
+});
 
 $app->run();
 
